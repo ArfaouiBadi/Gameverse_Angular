7f6ab9eca69f720f2dc4ac3580943fa1c3c8c056
ArfaouiBadi <arfaouibadi19@gmail.com>
2023-10-23 11:10:54 +0100

    Ex1 Formaulaire

--- src/app/AppModule.ts
+++ src/app/AppModule.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-
-import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -14,18 +15,35 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MyCartComponent } from './components/my-cart/my-cart.component';
 import { UsersComponentComponent } from './components/users-component/users-component.component';
 import { AccueilComponentComponent } from './components/accueil-component/accueil-component.component';
+import { FormTestComponent } from './components/form-test/form-test.component';
 
-const routes: Routes = [
-  {path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent},
-  {path:'dashboard',component:DashboardComponent},
-  {path:'myCart',component:MyCartComponent},
-  {path:'User',component:UsersComponentComponent},
-  {path:'Acceuil',component:MainContainerComponent},
-];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainContainerComponent,
+    CarouselComponent,
+    CardsComponent,
+    LoginComponent,
+    SignupComponent,
+    StoreComponent,
+    DashboardComponent,
+    MyCartComponent,
+    UsersComponentComponent,
+    AccueilComponentComponent,
+    FormTestComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppRoutingModule { }
+
+export class AppModule {
+}
--- src/app/components/form-test/form-test.component.ts
+++ src/app/components/form-test/form-test.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { Personne } from 'src/app/interfaces/personne';
 @Component({
   selector: 'app-form-test',
   templateUrl: './form-test.component.html',
   styleUrls: ['./form-test.component.css']
 })
-export class FormTestComponent {
+export class FormTestComponent implements OnInit{
+  personnes: Array<Personne> = []; 
+  personne: Personne = {};
+  constructor() { }
+  ngOnInit(): void { }
+  ajouterPersonne(): void { 
+      this.personnes.push({ ...this.personne 
+    });
+  this.personne.nom = ""; 
+  this.personne.prenom = "";
+  console.log(this.personnes);
+}
 
+  
 }
