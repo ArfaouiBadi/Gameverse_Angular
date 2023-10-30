4195e81fe9f4b28b062bbc54f6afebe76e411fa5
ArfaouiBadi <arfaouibadi19@gmail.com>
2023-10-30 18:52:56 +0100

    feat:added signup component :it stocks data in to an array personne using formBuilder

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, EmailValidator } from "@angular/forms";
+
+interface Personne {
+  userName: string;
+  nom: string;
+  prenom: string;
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +15,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  personnes: Personne[] = [];
+  personneForm = this.fb.group({
+    userName: ["", Validators.required],
+    nom: ["", Validators.required],
+    prenom: ["", [Validators.required, Validators.minLength(2)]],
+    email: ["", Validators.required],
+    password: ["", Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void { }
+  ajouterPersonne(): void {
+    this.personnes.push({ ...this.personneForm.value } as Personne);
+    console.log(this.personnes);
+    this.personneForm.reset();
+  }
+  afficherTout(): void { console.log(this.personnes); }
 }
+
+
+
