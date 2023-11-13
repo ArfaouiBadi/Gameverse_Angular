f725775e2e970f63c221aeb1d62c7e5e49b7dd3b
ArfaouiBadi <arfaouibadi19@gmail.com>
2023-11-13 10:47:36 +0100

    fixed personne service and getpersonne method

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
 import { Validators, FormBuilder, EmailValidator } from "@angular/forms";
 import { PersonneService } from 'src/app/services/personne.service';
-interface Personne {
-  userName: string;
-  nom: string;
-  prenom: string;
-  email: string;
-  password: string;
-}
-
+import { Personne } from 'src/app/interfaces/personne';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  personnes: Personne[] = [];
+  personnes: Array <Personne> = new Array<Personne>();
   personneForm = this.fb.group({
     userName: ["", Validators.required],
     nom: ["", Validators.required],
@@ -23,14 +16,13 @@ export class SignupComponent {
     email: ["", Validators.required],
     password: ["", Validators.required],
   });
-
   constructor(private fb: FormBuilder,private personneService:PersonneService) { }
   ngOnInit(): void {
     this.getPersonneData();
   }
   getPersonneData(): void {
-    this.personneService.getAll();
-    console.log(this.personnes);
+    this.personneService.getAll().subscribe(data => {this.personnes.push( ...data )})
+    console.log(this.personnes[0]);
     }
   ajouterPersonne(): void {
     this.personnes.push({ ...this.personneForm.value } as Personne)
