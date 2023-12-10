3c8d078387822c79912d0b83911aa8cb57862f77
ArfaouiBadi <arfaouibadi19@gmail.com>
2023-12-10 22:42:49 +0100

    working on clips CRUD

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Clips } from 'src/app/interfaces/clips';
 import { Games } from 'src/app/interfaces/games';
 import { Personne } from 'src/app/interfaces/personne';
@@ -11,39 +12,69 @@ import { PersonneService } from 'src/app/services/personne.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit{
+  
+
+  addClipForm: FormGroup;
   personne :Personne[] =[];
   clips :Clips[] =[];
-  constructor(private personneService:PersonneService,private clipsService:ClipsService) { }
+  constructor(private personneService:PersonneService,private clipsService:ClipsService,private formBuilder: FormBuilder) {
+    this.addClipForm = this.formBuilder.group({
+      id: [''],
+      title: [''],
+      description: [''],
+      game: [''],
+      views: [''],
+      likes: [''],
+      dislikes: [''],
+      video: [''],
+      image: [''],
+      userName: ['']
+    });
+   }
   
   ngOnInit(): void {
     this.personneService.getPersonne(localStorage.getItem("userConnected")!).subscribe(
       (data) => {
         this.personne = data;
+        this.clips = this.personne[0].clips!;
       },
       (error) => {
         console.error('Error fetching personne:', error);
       }
     );
-    this.clipsService.getClips(localStorage.getItem("userConnected")!).subscribe(
-      (data) => {
-        this.clips = data;
-      },
-      (error) => {
-        console.error('Error fetching Clips:', error);
-      }
-    );
-    console.log();
+    
+    
   }
   handleLogout(){
     localStorage.setItem("isConnected", "false");
     localStorage.setItem("userConnected", "");
-    
   }
 
   handleRemoveFromLibrary(game: Games) {
     const index = this.personne[0].library?.indexOf(game);
     console.log(index);
     this.personne[0].library?.splice(index!, 1);
+    this.personneService.updatePersonne(this.personne[0]).subscribe();
+  }
+  addClip(): void {
+    this.clipsService.addClip(this.addClipForm.value).subscribe(
+      (data) => {
+        console.log(data);
+        this.clips.push(data);
+      },
+      (error) => {
+        console.error('Error adding clip:', error);
+      }
+    );
+    this.personne[0].clips?.push(this.addClipForm.value);
+    this.personneService.updatePersonne(this.personne[0]).subscribe();
+  }
+  deleteClip(clip: Clips) {
+    const index = this.clips.indexOf(clip);
+    this.clips.splice(index, 1);
+    this.clipsService.deleteClip(clip).subscribe();
+    const index2 = this.personne[0].clips?.indexOf(clip);
+    this.personne[0].clips?.splice(index2!, 1);
     this.personneService.updatePersonne(this.personne[0]).subscribe();
   }
   
